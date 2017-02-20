b433d6db46b133773e1dfed45d042b7827643950
Mansi M. Shah <mmshah1990@gmail.com>
2017-02-20 16:56:16 +0530

    Added model validation

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer < ApplicationRecord
+
   mount_uploader :attachment, AttachmentUploader
+
+  validates_presence_of :email_to, :email_from, :attachment
+  validate :image_size_validation
+
+  def image_size_validation
+    if attachment.size > 2.gigabytes
+      errors.add(:base, "Image should be less than 2GB")
+    end
+  end
+
 end
