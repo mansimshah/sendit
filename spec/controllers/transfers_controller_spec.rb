2bdfb9f91b0c0619cfb156f9e81eddf0b016618c
Mansi M. Shah <mmshah1990@gmail.com>
2017-02-25 13:19:03 +0530

    Rspec for rendering form

--- spec/controllers/transfers_controller_spec.rb
+++ spec/controllers/transfers_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TransfersController, type: :controller do
 
+  let(:transfer) { FactoryGirl.attributes_for(:transfer) }
+
+  describe "GET new" do
+    it "renders :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
 end
--- spec/factories/transfers.rb
+++ spec/factories/transfers.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :transfer do
-    
+    email_to   { FFaker::Internet.email }
+    email_from   { FFaker::Internet.email }
+    attachment { File.new("#{Rails.root}/spec/support/fixtures/ruby.png") }
   end
 end
