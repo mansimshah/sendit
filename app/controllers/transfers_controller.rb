fc96b13f7a03c5c31c43494730a994164e805deb
Mansi M. Shah <mmshah1990@gmail.com>
2017-06-23 20:01:42 +0530

    Enabled multiple files download feature

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 class TransfersController < ApplicationController
 
   before_action :get_transfer, only: [:download_file, :download_all_files]
@@ -37,14 +38,30 @@ class TransfersController < ApplicationController
   end
 
   def download_all_files
+    folder_path = "#{Rails.root}/public/downloads/"
     zipfile_name = "#{Rails.root}/public/archive.zip"
-    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-      @transfer.transfer_attachments.each do |attachment|
-        zipfile.add(attachment.avatar.file.filename,attachment.avatar.url)
+
+    FileUtils.remove_dir(folder_path) if Dir.exist?(folder_path)
+    FileUtils.remove_entry(zipfile_name) if File.exist?(zipfile_name)
+
+    Dir.mkdir("#{Rails.root}/public/downloads")
+
+    @transfer.transfer_attachments.each do |attachment|
+      open(folder_path + "#{attachment.avatar.file.filename}", 'wb') do |file|
+        file << open("#{attachment.avatar.url}").read
       end
-      zipfile.get_output_stream("success") { |os| os.write "All done successfully" }
     end
+
+    input_filenames = Dir.entries(folder_path).select {|f| !File.directory? f}
+
+    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+      input_filenames.each do |attachment|
+        zipfile.add(attachment,File.join(folder_path,attachment))
+      end
+    end
+
     send_file(File.join("#{Rails.root}/public/", 'archive.zip'), :type => 'application/zip', :filename => "#{Time.now.to_date}.zip")
+
   end
 
   private
